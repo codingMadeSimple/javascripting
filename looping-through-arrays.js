fa0727928ec1b8db94f2f9d5a1ad3502c46f1c09
Colin Spangelo <colin.spangelo@gmail.com>
2023-05-08 15:55:28 -0600

    did extra looping array question

---
+++ looping-through-arrays.js
@@ -0,0 +1,5 @@
+const pets = ['cat', 'dog', 'rat'];
+for(let i = 0; i < pets.length; i++){
+  pets[i] = pets[i] + 's' //This line will add an 's' to the end of each element of the array
+}
+console.log(pets);
